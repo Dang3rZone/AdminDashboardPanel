eb13e82484b953bfb194f08f2668f837205eb4df
Andres Rosales <andresrosalespinzon@gmail.com>
2021-02-21 11:33:27 +0100

    working on the sidebar, the style and dashboard

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -6,10 +6,17 @@ import { RouterModule } from '@angular/router';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [DefaultComponent, DashboardComponent, PostsComponent],
   exports: [MatSidenavModule],
-  imports: [CommonModule, RouterModule, SharedModule, MatSidenavModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+  ],
 })
 export class DefaultModule {}
